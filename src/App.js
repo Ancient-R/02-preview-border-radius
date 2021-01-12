6a7d93817fb0f93bff6e3aa4cbd3d79c75fd9f7f
Ancient-R <navalimaricardo@gmail.com>
2021-01-12 13:38:02 -0600

    Definiendo el state de los bordes, mostrando el componente BorderComponent y BorderPreview

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
+import BorderComponent from './components/BorderComponent';
+import BorderPreview from './components/BorderPreview';
 
 function App() {
+
+  const [borders, setBorders] = useState({
+    topLeft: '',
+    topRight: '',
+    bottomLeft: '',
+    bottomRight: ''
+  });
+
   return (
-    <h1>Hola mundo desde React</h1>
+    <div className="container">
+      <BorderComponent 
+        borders={ borders }
+        setBorders={ setBorders }
+      />
+      <BorderPreview 
+        borders={ borders }
+      />
+    </div>
   );
 }
 
