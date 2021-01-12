eaa49ee3565e828acc5db6a2c854172e3bf53d96
Ancient-R <navalimaricardo@gmail.com>
2021-01-12 16:01:49 -0600

    Agregando un boton para copiar el estilo del border-radius

--- src/components/BorderClasses.jsx
+++ src/components/BorderClasses.jsx
@@ -1,24 +1,57 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
 const BorderClasses = ({ borders }) => {
 
+    const refClass = useRef();
+    const toastRef = useRef();
+
     const { topLeft, topRight, bottomLeft, bottomRight } = borders;
 
+    const handleCopy = () => {
+        if( refClass.current.textContent.trim() !== ''){
+            const styless = refClass.current.textContent.split('E');
+    
+            navigator.clipboard.writeText(styless[0]);
+    
+            toastRef.current.textContent= "Copiado"
+            toastRef.current.classList.add('copied', 'fadein');
+    
+            setTimeout(() => {
+                toastRef.current.classList.remove('copied', 'fadein')
+                toastRef.current.textContent= null
+            }, 3000);
+        }
+    }
     return (
         <>
-            { ( topLeft === topRight && topLeft === bottomLeft && topLeft === bottomRight ) ? 
-                <p>border-radius: { topLeft > 0 ? `${ topLeft }px;` : null } </p>
-            :
-                ( topLeft === bottomRight && topRight === bottomLeft ) ? 
-                <p>border-radius: { `${ topLeft }px` } { topRight > 0 ? `${ topRight }px;` : '0;' }</p> 
-            :
-                <p>border-radius: 
-                    { topLeft > 0 ? ` ${ topLeft }px ` : ' 0 ' } 
-                    { topRight > 0 ? `${ topRight }px ` : ' 0 ' } 
-                    { bottomLeft > 0 ? `${ bottomLeft }px ` : ' 0 ' } 
-                    { bottomRight > 0 ? `${ bottomRight }px; ` : ' 0 ' } 
-                </p>
-            }
+            <div 
+                className="container-class"
+                ref={ refClass }
+            >
+                { ( topLeft === topRight && topLeft === bottomLeft && topLeft === bottomRight ) ? 
+                    <p> { topLeft > 0 ? `border-radius: ${ topLeft }px;` : null } </p>
+                :
+                    ( topLeft === bottomRight && topRight === bottomLeft ) ? 
+                    <p>border-radius: { `${ topLeft }px` } { topRight > 0 ? `${ topRight }px;` : '0;' }</p> 
+                :
+                    <p>border-radius: 
+                        { topLeft > 0 ? ` ${ topLeft }px ` : ' 0 ' } 
+                        { topRight > 0 ? `${ topRight }px ` : ' 0 ' } 
+                        { bottomLeft > 0 ? `${ bottomLeft }px ` : ' 0 ' } 
+                        { bottomRight > 0 ? `${ bottomRight }px; ` : ' 0 ' } 
+                    </p>
+                }
+                <button 
+                    className="button-copy"
+                    onClick={ handleCopy }
+                >
+                    <i className="fas fa-copy"></i>
+                </button>
+
+                <p 
+                    ref={ toastRef }
+                ></p>
+            </div>
         </>
     );
 }
