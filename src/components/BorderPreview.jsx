f4227923f9c102bebb0167428459017b57203607
Ancient-R <navalimaricardo@gmail.com>
2021-01-12 12:31:35 -0600

    Componente que previzualizara el border radius

---
+++ src/components/BorderPreview.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const BorderPreview = () => {
+    return (  
+        <div className="border-preview">
+            <div className="border-code">
+                <p>border-radius: 10px 5px 7px 2px</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default BorderPreview;
