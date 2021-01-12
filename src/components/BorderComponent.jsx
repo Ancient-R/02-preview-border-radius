b6d6739a5bab905c4353db39c10caa82ce8e1470
Ancient-R <navalimaricardo@gmail.com>
2021-01-12 12:22:17 -0600

    Formulario para los bordes

--- src/components/BorderComponent.jsx
+++ src/components/BorderComponent.jsx
@@ -3,7 +3,38 @@ import React from 'react'
 const BorderComponent = () => {
     return (
         <form className="form">
+            <div className="form-group">
+                <label htmlFor="" className="form-label">Border top lef</label>
+                <input 
+                    type="number" 
+                    className="form-input"
+                />
+            </div>
             
+            <div className="form-group">
+                <label htmlFor="" className="form-label">Border top rigth</label>
+                <input 
+                    type="number" 
+                    className="form-input"
+                />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="" className="form-label">Border bottom lef</label>
+                <input 
+                    type="number" 
+                    className="form-input"
+                />
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="" className="form-label">Border bottom rigth</label>
+                <input 
+                    type="number" 
+                    className="form-input"
+                />
+            </div>
+
         </form>
     );
 }
